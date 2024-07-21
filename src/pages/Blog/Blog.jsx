94053c9754702865bc7144185692f34a0b2ee51d
ifunga ndana <ifungandana@gmail.com>
2024-07-21 20:27:42 +0200

    Added Blog page content

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -1,3 +1,18 @@
 export const Blog = () => {
-  return <>Blog</>;
+  return (
+    <div>
+      <p>
+        I have a presence on&nbsp;
+        <a href="https://medium.com/@ifungandana" target="_target">
+          Medium
+        </a>
+        . My older content is on&nbsp;
+        <a href="https://subiyacryolite.github.io/" target="_target">
+          https://subiyacryolite.github.io/
+        </a>
+        .
+      </p>
+      <hr />
+    </div>
+  );
 };
