2b1356c0cce11e3190ed8461f2292caeb35ac35f
Ifunga Ndana <ifungandana@gmail.com>
2024-08-03 19:33:19 +0200

    Cleared all compiler errors

--- src/context/withTabContext.tsx
+++ src/context/withTabContext.tsx
@@ -2,10 +2,7 @@ import { useEffect, FC, ComponentType, useContext } from "react";
 
 import { TabContext } from "./TabContext";
 
-export const withTabContext = <P extends object>(
-  Component: ComponentType<P>,
-  tab: string
-): FC<P> => {
+export const withTabContext = (Component: ComponentType, tab: string): FC => {
   const WithTabContext = () => {
     const { setActive } = useContext(TabContext);
 
