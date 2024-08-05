7631ebeca7efeb6a6387d42e13e4220e37b06cc1
ifunga ndana <ifungandana@gmail.com>
2024-08-05 15:49:15 +0200

    Adding additinal breakpoints

--- src/pages/Comics/Comics.tsx
+++ src/pages/Comics/Comics.tsx
@@ -1,10 +1,14 @@
-import { Tab } from "../../constants";
-import { withTabContext } from "../../context";
+import { Tab } from '../../constants';
+import { withTabContext } from '../../context';
 
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 
 const Impl: React.FC = () => {
-  return <Outlet />;
+  return (
+    <div className='main-container' style={{ marginTop: '1em' }}>
+      <Outlet />
+    </div>
+  );
 };
 
 export const Comics = withTabContext(Impl, Tab.Comics);
